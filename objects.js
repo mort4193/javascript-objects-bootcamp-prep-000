6feef43a940b0ea4b331f1d69b38a478e6f91e3a
jonathan Morton <jmorton4193@gmail.com>
2017-10-01 22:13:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ playlist = {name: 'title'}
 function updatePlaylist (playlist, artistName, songTitle){
   return Object.assign( {}, playlist, {[artistName]: songTitle});
 }
-var playlist = {artistName: 'songTitle'}
+
 function removeFromPlaylist (playlist, artistName) {
 
    delete playlist.artistName
