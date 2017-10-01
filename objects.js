1f25130d4723eb3533b2033b92307956d4eb3dd9
jonathan Morton <jmorton4193@gmail.com>
2017-10-01 22:21:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-playlist = {artistName: 'songTitle'}
+playlist = {'artistName': 'songTitle'}
 function updatePlaylist (playlist, artistName, songTitle){
   return Object.assign( {}, playlist, {[artistName]: songTitle});
 }
